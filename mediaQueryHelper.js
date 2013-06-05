960a2aa7f02c1fe8135db4f2f7dd5db12d81b2dd
Rusta <rusta@rusta.co.uk>
2013-06-05 06:41:22 +0100

    in progress

--- mediaQueryHelper.js
+++ mediaQueryHelper.js
@@ -33,7 +33,7 @@ ddo.addJqueryUI = function(){
 ddo.checkForLibraryReadiness = function(){	
 
 	// if jQuery exists then set relevant param to true
-	if (window.jQuery) { ddo.jQuery = true };
+	if (window.jQuery) { ddo.jQuery = true; clearInterval(ddo.loadLibraryTimer); };
 
 	// if jQuery doesn't exist and is not currently loading then add it
 	if (!ddo.jQuery && !ddo.jQueryLoading)
@@ -51,6 +51,7 @@ ddo.checkForLibraryReadiness = function(){
 		// if jQueryUI exists then set param to true and inialise the dimension display (finally)!
 		if (window.jQuery.ui) { 
 							ddo.jQueryUI = true; 
+							clearInterval(ddo.loadLibraryTimer);
 							ddo.initialiseDimensionsDisplay();
 						};
 
@@ -96,10 +97,17 @@ ddo.getDimensions = function(){
 
 // Add Dimension Display to page
 ddo.initialiseDimensionsDisplay = function(){
+
 	// we check for/add a class to the body to confirm to ensure we only add the dimension display div once
+
+
+    console.log( document.body.classList.contains('dimensionDisplayAdded') )
+
 	if(!$('body').hasClass('dimensionDisplayAdded'))
 	{
 		$('body').addClass('dimensionDisplayAdded');
+
+		console.log( document.body.classList.contains('dimensionDisplayAdded') )
 
 		// the html for our dimension display div
 		var dimensionDisplayHTML = '<div id="dimensionDisplay">Width: <span id="dimensionDisplayWidth"></span><br>Height: <span id="dimensionDisplayHeight"></span></div>';
@@ -122,10 +130,8 @@ ddo.initialiseDimensionsDisplay = function(){
 									'		cursor: move; ' +
 									'		opacity:0; ' +									
 									'		font-size: 80%; ' +
-									'		-webkit-transition: background 1s ease; ' +
-									'		transition: background 1s ease; ' +
-									'		-webkit-transition: opacity 1s ease; ' +
-									'		transition: opacity 1s ease; ' +
+									'		-webkit-transition: background 1s ease, opacity 1s ease; ' +
+									'		transition: background 1s ease, opacity 1s ease; ' +
 									'	}' +
 									'	#dimensionDisplay:hover { ' +
 									'		background: rgba(54, 25, 25, 0.7); ' +
